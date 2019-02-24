b979fffad4ce358181ec022df93a242f4ee125a2
Miguel Jaque <mjaque@migueljaque.com>
2019-02-24 19:41:13 +0100

    Quitados ;

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const HEIGHT = 800;
 // Carga de dependencias (librerías)
 var express = require('express');
 var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
+var path = require('path')
+var socketIO = require('socket.io')
 
 // Variables globales
-var app = express();			// Aplicación Express
-var server = http.Server(app);	// Servidor HTTP
-var io = socketIO(server);
-var players = {};				//Es un objeto. Cada socket.id es una propiedad que contiene a un jugador
-var numJug = 0;					//Número de jugadores registrados
+var app = express()				// Aplicación Express
+var server = http.Server(app)	// Servidor HTTP
+var io = socketIO(server)
+var players = {}				//Es un objeto. Cada socket.id es una propiedad que contiene a un jugador
+var numJug = 0					//Número de jugadores registrados
 var colors = ['red', 'green', 'blue', 'yellow', 'pink', 'white', 'orange']
 var dirs = ['u', 'r', 'd', 'l']	//Lista de direcciones
 
-app.use(express.static('static'));
+app.use(express.static('static'))
 
 // Routing
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname, 'static/tron.html'));
+	response.sendFile(path.join(__dirname, 'static/tron.html'))
 });
 
 // Iniciamos el servidor en el puerto 8080.
@@ -36,7 +36,7 @@ server.listen(8080, function () {
 // Manejadores de WebSockets.io
 io.on('connection', function(socket) {
 	socket.on('new', function(data) {
-		console.log("Registrado nuevo jugador: " + data);
+		console.log("Registrado nuevo jugador: " + data)
 		
 		players[socket.id] = {
 			name : data,
@@ -45,20 +45,20 @@ io.on('connection', function(socket) {
 			dir : dirs[parseInt(Math.random() * dirs.length)]
 		}
 		
-		var message = 'Se ha unido <strong>' + data + '</strong><br/>';
-		socket.emit('accepted');				//Enviamos solo al cliente
-		io.sockets.emit('message', message);	//Enviamos a todos
-		numJug++;
+		var message = 'Se ha unido <strong>' + data + '</strong><br/>'
+		socket.emit('accepted')					//Enviamos solo al cliente
+		io.sockets.emit('message', message)		//Enviamos a todos
+		numJug++
 	});
 	socket.on('message', function(data) {
-		console.log("Recibido mensaje: " + data);
-		if (players[socket.id]){	//Por si no existe el jugador (por reinicios){
-			var message = '<strong>' + players[socket.id].name + ':</strong> ' + data + '<br/>';
-			io.sockets.emit('message', message);	//Enviamos a todos
+		console.log("Recibido mensaje: " + data)
+		if (players[socket.id]){					//Por si no existe el jugador (por reinicios)
+			var message = '<strong>' + players[socket.id].name + ':</strong> ' + data + '<br/>'
+			io.sockets.emit('message', message)		//Enviamos a todos
 		}
-	});
+	})
 	//TODO: socket.on('turn', function(data){});
-});
+})
 
 //Envío de posiciones
 setInterval(function() {
--- static/js/tron.js
+++ static/js/tron.js
@@ -1,56 +1,56 @@
-"use strict";
+"use strict"
 
-console.log('Cargado juego.js');
+console.log('Cargado juego.js')
 
 //Constantes
-const WIDTH = 1000;
-const HEIGHT = 800;
-const GRID_WIDTH = 50;
-const GRID_HEIGHT = 50;
+const WIDTH = 1000
+const HEIGHT = 800
+const GRID_WIDTH = 50
+const GRID_HEIGHT = 50
 
 //Variables
-var canvas;
-var socket = io();	//Socket de comunicación con el servidor
+var canvas
+var socket = io()	//Socket de comunicación con el servidor
 
-window.onload = init;
+window.onload = init
 
 function init(){
-	canvas = document.getElementById('canvas');
-	canvas.width = WIDTH;
-	canvas.height = HEIGHT;
-	canvas.ctx = canvas.getContext('2d');
+	canvas = document.getElementById('canvas')
+	canvas.width = WIDTH
+	canvas.height = HEIGHT
+	canvas.ctx = canvas.getContext('2d')
 	
 	//TODO: Refactorizar todo el dibujo de la parrilla a una función drawGrid
 	//Dibujamos el fondo
-	canvas.ctx.beginPath();
-	canvas.ctx.rect(0, 0, WIDTH, HEIGHT);
-	canvas.ctx.fillStyle = "#09416C";
-	canvas.ctx.fill();
+	canvas.ctx.beginPath()
+	canvas.ctx.rect(0, 0, WIDTH, HEIGHT)
+	canvas.ctx.fillStyle = "#09416C"
+	canvas.ctx.fill()
 
 	//Dibujamos las líneas de la parrilla
 	for(let i = 1; i < WIDTH/GRID_WIDTH; i++){
-		canvas.ctx.strokeStyle = '#8EB3D0';	//Establecemos el color de lí­nea
-		canvas.ctx.lineWidth = 2;
-		canvas.ctx.moveTo(i*GRID_WIDTH, 0);
-		canvas.ctx.lineTo(i*GRID_WIDTH, HEIGHT);
-		canvas.ctx.stroke();
+		canvas.ctx.strokeStyle = '#8EB3D0'	//Establecemos el color de lí­nea
+		canvas.ctx.lineWidth = 2
+		canvas.ctx.moveTo(i*GRID_WIDTH, 0)
+		canvas.ctx.lineTo(i*GRID_WIDTH, HEIGHT)
+		canvas.ctx.stroke()
 	}
 	for(let i = 1; i < HEIGHT/GRID_HEIGHT; i++){
-		canvas.ctx.strokeStyle = '#8EB3D0';	//Establecemos el color de lí­nea
-		canvas.ctx.lineWidth = 2;
-		canvas.ctx.moveTo(0, i*GRID_HEIGHT);
-		canvas.ctx.lineTo(WIDTH, i*GRID_HEIGHT);
-		canvas.ctx.stroke();
+		canvas.ctx.strokeStyle = '#8EB3D0'	//Establecemos el color de lí­nea
+		canvas.ctx.lineWidth = 2
+		canvas.ctx.moveTo(0, i*GRID_HEIGHT)
+		canvas.ctx.lineTo(WIDTH, i*GRID_HEIGHT)
+		canvas.ctx.stroke()
 	}
 	
-	document.getElementById('btnRegister').onclick = register;
+	document.getElementById('btnRegister').onclick = register
 	
-	socket.on('accepted', registerAccepted);
-	socket.on('message', updateLog);
-	socket.on('state', updateGrid);
+	socket.on('accepted', registerAccepted)
+	socket.on('message', updateLog)
+	socket.on('state', updateGrid)
 	
 	//Para enviar al chat
-	window.onkeypress = keypressHandler;
+	window.onkeypress = keypressHandler
 	
 	//Para mover la moto
 	//TODO: Detectar la pulsación de teclas para girar la moto.
@@ -78,13 +78,13 @@ function register(event){
 
 function sendMessage(event){
 	socket.emit('message', document.getElementById('message').value)
-	document.getElementById('message').value = '';
+	document.getElementById('message').value = ''
 }
 
 function registerAccepted(){
-	console.log('Registro Aceptado');
-	document.getElementById('pRegister').style.display = 'none';
-	document.getElementById('chat').style.display = 'block';
+	console.log('Registro Aceptado')
+	document.getElementById('pRegister').style.display = 'none'
+	document.getElementById('chat').style.display = 'block'
 }
 
 function updateLog(data){
@@ -96,5 +96,5 @@ function updateLog(data){
 
 
 function dibujar(img, x1Img, y1Img, x2Img, y2Img, xCanvas, yCanvas){
-    canvas.ctx.drawImage(img, x1Img, y1Img, x2Img - x1Img, y2Img - y1Img, xCanvas, yCanvas, (x2Img - x1Img), (y2Img - y1Img));   
+    canvas.ctx.drawImage(img, x1Img, y1Img, x2Img - x1Img, y2Img - y1Img, xCanvas, yCanvas, (x2Img - x1Img), (y2Img - y1Img))
 }
