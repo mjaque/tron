fe6233b3a5c0de59eea05ba39638ef575368c71e
Miguel Jaque <mjaque@migueljaque.com>
2019-03-15 08:42:30 +0100

    Trayectorias de motos.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 //Constantes
 const WIDTH = 1000;
 const HEIGHT = 800;
+const VEL = 3;
 
 // Carga de dependencias (librerías)
 var express = require('express');
 var http = require('http');
-var path = require('path')
-var socketIO = require('socket.io')
+var path = require('path');
+var socketIO = require('socket.io');
 
 // Variables globales
-var app = express()				// Aplicación Express
-var server = http.Server(app)	// Servidor HTTP
-var io = socketIO(server)
-var players = {}				//Es un objeto. Cada socket.id es una propiedad que contiene a un jugador
-var numJug = 0					//Número de jugadores registrados
+var app = express();			// Aplicación Express
+var server = http.Server(app);	// Servidor HTTP
+var io = socketIO(server);
+var players = {};	//Es un objeto. Cada socket.id es una propiedad que contiene a un jugador
+var numJug = 0		//Número de jugadores registrados
+var dirs = ['u', 'r', 'd', 'l']
 var colors = ['red', 'green', 'blue', 'yellow', 'pink', 'white', 'orange']
-var dirs = ['u', 'r', 'd', 'l']	//Lista de direcciones
 
-app.use(express.static('static'))
+app.use(express.static('static'));
 
 // Routing
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname, 'static/tron.html'))
+	response.sendFile(path.join(__dirname, 'static/tron.html'));
 });
 
 // Iniciamos el servidor en el puerto 8080.
@@ -36,7 +37,10 @@ server.listen(8080, function () {
 // Manejadores de WebSockets.io
 io.on('connection', function(socket) {
 	socket.on('new', function(data) {
-		console.log("Registrado nuevo jugador: " + data)
+		console.log("Registrado nuevo jugador: " + data);
+		
+		//Creamos el nuevo jugador o le sobreescribimos is ya existe
+		//players[socket.id] = {name : data};
 		
 		players[socket.id] = {
 			name : data,
@@ -44,25 +48,61 @@ io.on('connection', function(socket) {
 			color : colors[numJug],
 			dir : dirs[parseInt(Math.random() * dirs.length)]
 		}
+		//Insertamos la copia
+		players[socket.id].trace.push([players[socket.id].trace[0][0], players[socket.id].trace[0][1]])
+		console.log(players[socket.id])
 		
-		var message = 'Se ha unido <strong>' + data + '</strong><br/>'
-		socket.emit('accepted')					//Enviamos solo al cliente
-		io.sockets.emit('message', message)		//Enviamos a todos
+		var message = 'Se ha unido <strong>' + data + '</strong><br/>';
+		socket.emit('accepted');				//Enviamos solo al cliente
+		io.sockets.emit('message', message);	//Enviamos a todos
 		numJug++
 	});
 	socket.on('message', function(data) {
-		console.log("Recibido mensaje: " + data)
-		if (players[socket.id]){					//Por si no existe el jugador (por reinicios)
-			var message = '<strong>' + players[socket.id].name + ':</strong> ' + data + '<br/>'
-			io.sockets.emit('message', message)		//Enviamos a todos
+		console.log("Recibido mensaje: " + data);
+		if (players[socket.id]){	//Por si no existe el jugador (por reinicios){
+			var message = '<strong>' + players[socket.id].name + ':</strong> ' + data + '<br/>';
+			io.sockets.emit('message', message);	//Enviamos a todos
+		}
+	});
+	socket.on('turn', function(dir){
+		console.log('turn: ' + dir)
+		if (players[socket.id]){
+			var player = players[socket.id]
+			player.dir = dir
+			var ultimaPosicion = player.trace[player.trace.length - 1]
+			console.log('turn2: ' + ultimaPosicion[0] + ", " + ultimaPosicion[1])
+			players[socket.id].trace.push( [ultimaPosicion[0], ultimaPosicion[1]] )
 		}
 	})
-	//TODO: socket.on('turn', function(data){});
-})
+});
 
 //Envío de posiciones
 setInterval(function() {
-	//TODO: updatePositions()
-	io.sockets.emit('state', players)
-//}, 1000 / 10)
-}, 2000)
+	updatePositions()
+	io.sockets.emit('update', players)
+}, 1000 / 10)
+//}, 1000)
+
+
+function updatePositions(){
+	for (var id in players){
+		var player = players[id]
+		var ultimaPosicion = player.trace[player.trace.length - 1]
+		switch(player.dir){
+			case 'u':
+				ultimaPosicion[1] -= VEL
+				break;
+			case 'd':
+				ultimaPosicion[1] += VEL
+				break;
+			case 'l':
+				ultimaPosicion[0] -= VEL
+				break;
+			case 'r':
+				ultimaPosicion[0] += VEL
+				break;
+		}
+		//console.log(player.trace)
+	}
+}
+
--- static/js/tron.js
+++ static/js/tron.js
@@ -1,64 +1,85 @@
-"use strict"
+"use strict";
 
-console.log('Cargado juego.js')
+console.log('Cargado juego.js');
 
 //Constantes
-const WIDTH = 1000
-const HEIGHT = 800
-const GRID_WIDTH = 50
-const GRID_HEIGHT = 50
+const WIDTH = 1000;
+const HEIGHT = 800;
+const GRID_WIDTH = 50;
+const GRID_HEIGHT = 50;
 
 //Variables
-var canvas
-var socket = io()	//Socket de comunicación con el servidor
+var canvas;
+var socket = io();	//Socket de comunicación con el servidor
 
-window.onload = init
+window.onload = init;
 
 function init(){
-	canvas = document.getElementById('canvas')
-	canvas.width = WIDTH
-	canvas.height = HEIGHT
-	canvas.ctx = canvas.getContext('2d')
+	canvas = document.getElementById('canvas');
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+	canvas.ctx = canvas.getContext('2d');
 	
-	//TODO: Refactorizar todo el dibujo de la parrilla a una función drawGrid
+	drawGrid();
+	
+	document.getElementById('btnRegister').onclick = register;
+	
+	socket.on('accepted', registerAccepted);
+	socket.on('message', updateLog);
+	socket.on('update', updateGrid);
+	
+	//Para enviar al chat
+	window.onkeypress = keypressHandler;
+	
+	//Para mover la moto
+	window.onkeydown = keydownHandler;
+}
+
+function drawGrid(){
 	//Dibujamos el fondo
-	canvas.ctx.beginPath()
-	canvas.ctx.rect(0, 0, WIDTH, HEIGHT)
-	canvas.ctx.fillStyle = "#09416C"
-	canvas.ctx.fill()
+	canvas.ctx.beginPath();
+	canvas.ctx.rect(0, 0, WIDTH, HEIGHT);
+	canvas.ctx.fillStyle = "#09416C";
+	canvas.ctx.fill();
 
 	//Dibujamos las líneas de la parrilla
 	for(let i = 1; i < WIDTH/GRID_WIDTH; i++){
-		canvas.ctx.strokeStyle = '#8EB3D0'	//Establecemos el color de lí­nea
-		canvas.ctx.lineWidth = 2
-		canvas.ctx.moveTo(i*GRID_WIDTH, 0)
-		canvas.ctx.lineTo(i*GRID_WIDTH, HEIGHT)
-		canvas.ctx.stroke()
+		canvas.ctx.strokeStyle = '#8EB3D0';	//Establecemos el color de lí­nea
+		canvas.ctx.lineWidth = 2;
+		canvas.ctx.moveTo(i*GRID_WIDTH, 0);
+		canvas.ctx.lineTo(i*GRID_WIDTH, HEIGHT);
+		canvas.ctx.stroke();
 	}
 	for(let i = 1; i < HEIGHT/GRID_HEIGHT; i++){
-		canvas.ctx.strokeStyle = '#8EB3D0'	//Establecemos el color de lí­nea
-		canvas.ctx.lineWidth = 2
-		canvas.ctx.moveTo(0, i*GRID_HEIGHT)
-		canvas.ctx.lineTo(WIDTH, i*GRID_HEIGHT)
-		canvas.ctx.stroke()
+		canvas.ctx.strokeStyle = '#8EB3D0';	//Establecemos el color de lí­nea
+		canvas.ctx.lineWidth = 2;
+		canvas.ctx.moveTo(0, i*GRID_HEIGHT);
+		canvas.ctx.lineTo(WIDTH, i*GRID_HEIGHT);
+		canvas.ctx.stroke();
 	}
-	
-	document.getElementById('btnRegister').onclick = register
-	
-	socket.on('accepted', registerAccepted)
-	socket.on('message', updateLog)
-	socket.on('state', updateGrid)
-	
-	//Para enviar al chat
-	window.onkeypress = keypressHandler
-	
-	//Para mover la moto
-	//TODO: Detectar la pulsación de teclas para girar la moto.
+
 }
 
-function updateGrid(data){
-	//console.log(data)
-	//TODO: Mostrar la trayectoria de cada jugador
+function updateGrid(players){
+	//console.log(players)
+	drawGrid()
+	for (var id in players){
+		var player = players[id]
+		//console.log("name: " + player.name)
+		//console.log("length: " + player.trace.length)
+		canvas.ctx.fillStyle = player.color;
+		for (let i = 1; i < player.trace.length; i++){
+			var pointA = player.trace[i-1]
+			var pointB = player.trace[i]
+			var x = Math.min(pointA[0], pointB[0])
+			var y = Math.min(pointA[1], pointB[1])
+			var width = Math.max(5, Math.abs(pointB[0] - pointA[0]))
+			var height = Math.max(5, Math.abs(pointB[1] - pointA[1]))
+			//console.log("width: " + width + " ; height: " + height)
+			canvas.ctx.fillRect(x, y, width, height)
+			//console.log(x + ", " + y + ", " + width + ", " + height)
+		}
+	}
 }
 
 function keypressHandler(event){
@@ -71,6 +92,35 @@ function keypressHandler(event){
 	}
 }
 
+function keydownHandler(event){
+	//console.log('Flecha: ' + event.keyCode)
+	
+	//TODO: Procesar solo si el jugador está registrado
+	
+	var turn = null
+	switch(event.keyCode){
+		case 37:		//	Left
+			turn = 'l'
+			break;
+		case 38:		//	Up
+			turn = 'u'
+			break;
+		case 39:		//	Right
+			turn = 'r'
+			break;
+		case 40:		//	Down
+			turn = 'd'
+			break;
+	}
+
+	if (turn != null){
+		socket.emit('turn', turn)
+		//Evitamos que el evento siga
+		event.preventDefault()
+		event.stopPropagation()
+	}
+}
+
 function register(event){
 	console.log('Registrando Jugador');
 	socket.emit('new', document.getElementById('name').value)
@@ -78,13 +128,13 @@ function register(event){
 
 function sendMessage(event){
 	socket.emit('message', document.getElementById('message').value)
-	document.getElementById('message').value = ''
+	document.getElementById('message').value = '';
 }
 
 function registerAccepted(){
-	console.log('Registro Aceptado')
-	document.getElementById('pRegister').style.display = 'none'
-	document.getElementById('chat').style.display = 'block'
+	console.log('Registro Aceptado');
+	document.getElementById('pRegister').style.display = 'none';
+	document.getElementById('chat').style.display = 'block';
 }
 
 function updateLog(data){
@@ -96,5 +146,5 @@ function updateLog(data){
 
 
 function dibujar(img, x1Img, y1Img, x2Img, y2Img, xCanvas, yCanvas){
-    canvas.ctx.drawImage(img, x1Img, y1Img, x2Img - x1Img, y2Img - y1Img, xCanvas, yCanvas, (x2Img - x1Img), (y2Img - y1Img))
+    canvas.ctx.drawImage(img, x1Img, y1Img, x2Img - x1Img, y2Img - y1Img, xCanvas, yCanvas, (x2Img - x1Img), (y2Img - y1Img));   
 }
